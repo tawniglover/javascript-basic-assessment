2340db51117b36c3d30beca8fffe4bd376553b57
equinoxjenkins <equinoxjenkins@gmail.com>
2016-11-18 15:02:00 -0700

    complete

--- assessment.js
+++ assessment.js
@@ -1,4 +1,3 @@
-
 // #1 Create a variable called hello and assign it the string 'goodbye'
 
 var hello = 'goodbye';
@@ -16,27 +15,30 @@ var iLoveCode = iLove + ' code';
 // bob likes apples, bananas, and cherries (array of strings)
 
 var bob = {
-	height: '6ft',
-	age: 24,
-	hair: {style: 'spikey', color: 'brown'},
-	likes: ['apples', 'bananas', 'cherries'],
-	presidentOfTheUnitedStates: false
+    height: '6ft',
+    age: 24,
+    hair: {
+        style: 'spikey',
+        color: 'brown'
+    },
+    likes: ['apples', 'bananas', 'cherries'],
+    presidentOfTheUnitedStates: false
 };
 
 
 
 // #4 Change my shirt color to pink using dot notation
 var myShirt = {
-	type: 'polo',
-	color: 'red'
+    type: 'polo',
+    color: 'red'
 };
 
 myShirt.color = 'pink';
 
 // Change my shirt type to spandex using square bracket notation
 var myOtherShirt = {
-	type: 'polo',
-	color: 'red'
+    type: 'polo',
+    color: 'red'
 };
 
 myOtherShirt["type"] = 'spandex';
@@ -46,29 +48,29 @@ myOtherShirt["type"] = 'spandex';
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
 
 var zoo = {
-	monkeys: 8,
-	giraffes: 4,
-	elephants: 2
+    monkeys: 8,
+    giraffes: 4,
+    elephants: 2
 };
 
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
 var snake = {
-	sliters: 'sideways',
-	eats: 'rodents',
-	says: 'ssss',
-	smells: 'heat',
-	runs: 'legless'
+    sliters: 'sideways',
+    eats: 'rodents',
+    says: 'ssss',
+    smells: 'heat',
+    runs: 'legless'
 };
 
 
-function looper (snake) {
-	for (var s in snake) {
-		if (s[0] === 's') {
-			snake[s] = 's';
-		}
-	}
+function looper(snake) {
+    for (var s in snake) {
+        if (s[0] === 's') {
+            snake[s] = 's';
+        }
+    }
 }
 
 looper(snake);
@@ -102,19 +104,19 @@ var thirdHero = heroes[2];
 // the array.
 
 function addItem(array, item) {
-	array.push(item);
-	return array;
+    array.push(item);
+    return array;
 }
 
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
 
-function removeItem (array, string) {
-	for (var i = array.length - 1; i >= 0; i--) {
-		if (array[i] === string) {
-			array.splice(i, 1);
-		}
-	}
-	return array;
+function removeItem(array, string) {
+    for (var i = array.length - 1; i >= 0; i--) {
+        if (array[i] === string) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 }
 
 // Removes all instances of that string from the array. And return the modified array.
@@ -124,33 +126,51 @@ function removeItem (array, string) {
 // #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
 // repeats strings. example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
 
-function doubleTheFun (parameter) {
-	if (Number(parameter)) {
-		parameter = (Number(parameter) * 2);
-	} else if (typeof parameter === 'string' || parameter instanceof String) {
-		parameter = parameter.repeat(2);
-	}
-	return parameter;
+function doubleTheFun(parameter) {
+    if (Number(parameter)) {
+        parameter = (Number(parameter) * 2);
+    } else if (typeof parameter === 'string' || parameter instanceof String) {
+        parameter = parameter.repeat(2);
+    }
+    return parameter;
 }
 
 // #12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
 // return the value from the object that corresponds to the property
 
-function getValueOfProperty (obj, name) {
-	if (obj.hasOwnProperty(name)) {
-		return name;
-	}
+function getValueOfProperty(obj, name) {
+    if (obj.hasOwnProperty(name)) {
+        return obj[name];
+    }
 }
 
 // #13 Write a function called makeChatMessage that takes in a message and author as parameters
 // and returns an object with a message, author, and timestamp, that is
 // the current time as a Date object
 
-function makeChatMessage (message, author) {
-	return {message: (message),  author: (author), date: (Date.now)};
+function makeChatMessage(message, author) {
+    return {
+        message: message,
+        author: author,
+        timestamp: new Date()
+    };
 }
 
 // #14 Create a function called coderTest that takes in an object that is a person. It looks to see if the person’s name is Jeremy and then changes the person object to have a property called lovesCode with a value of 10.  If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
+
+var person = {
+    name: Jeremy
+};
+
+function coderTest(person) {
+    if (person.name === 'Jeremy') {
+        person.lovesCode = 10;
+    } else if (person.name === 'Brack') {
+        person.lovesCode = 0;
+    } else {
+        person.lovesCode = 5;
+    }
+}
 
 
 // #15 Create a function called outside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
@@ -162,5 +182,24 @@ function makeChatMessage (message, author) {
     Otherwise - return "I love outside"
 */
 
+function outside (temperature, humidity, cloudiness) {
+	if (temperature > 80 && humidity > 40) {
+		return "I'm all sweat";
+	} else if (temperature < 40 && cloudiness > 60) {
+		return "I have icecicles";
+	} else if (temperature > 80 && humidity < 40 && cloudiness < 20) {
+		return "I'm literally in the desert";
+	} else if (temperature > 80 || humidity > 50 || cloudiness > 50) {
+		return "Hmm, probably not";
+	} else {
+		return "I love outside";
+	}
+}
+
 // #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the argument string(back) + ' back'."
 // example - If I call you with 'Give it' you should invoke holla with 'Give it back'
+
+
+function callerBack (holla, back) {
+	holla(back + ' back');
+}
